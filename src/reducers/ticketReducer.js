22a9d96579003e916d4ba04d12717d6eaa4a9139
mittalsuthar <124027054+mittalsuthar@users.noreply.github.com>
2024-06-29 19:45:09 +0530

    first commit

---
+++ src/reducers/ticketReducer.js
@@ -0,0 +1,47 @@
+import { act } from "react";
+
+export default function ticketReducer(state, action) {
+  switch (action.type) {
+    case "ADD_TICKET":
+      return { ...state, tickets: [...state.tickets, action.payload] };
+    case "UPDATE_TICKET":
+      return {
+        ...state,
+        tickets: state.tickets.map((ticket) =>
+          ticket.id === action.payload.id ? action.payload : ticket
+        ),
+        editingTicket: null,
+      };
+    case "DELETE_TICKET":
+      if (state.editingTicket && state.editingTicket.id === action.payload.id) {
+        return {
+          ...state,
+          tickets: state.tickets.filter(
+            (ticket) => ticket.id !== action.payload.id
+          ),
+          editingTicket: null,
+        };
+      } else {
+        return {
+          ...state,
+          tickets: state.tickets.filter(
+            (ticket) => ticket.id !== action.payload.id
+          ),
+        };
+      }
+    case "Set_Editing_Ticket":
+      return {
+        ...state,
+        editingTicket: action.payload,
+      };
+
+    case "Clear_Editing_Ticket":
+      return {
+        ...state,
+        editingTicket: null,
+      };
+
+    default:
+      return state;
+  }
+}
